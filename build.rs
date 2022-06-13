9ad3696e3ca8b0bc0a84fe0d648a57a7458cbdf6
Frank Wolf <fwolf@posteo.de>
2022-06-13 17:10:39 +0200

    feat: a starter project for using rust with SDL2

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+    println!("cargo:rustc-link-search=framework=/Library/Frameworks");
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+}
